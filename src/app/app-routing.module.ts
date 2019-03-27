cbf0c3535edd78258fd5c4ed62cdfbde8af624aa
Jurek <jurek2006@gmail.com>
2019-03-27 11:42:35 +0100

    contact list component showing for signing in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { CourseEditComponent } from "./courses/course-edit/course-edit.component
 import { TestComponent } from "./test/test.component";
 import { ContactsManageComponent } from "./contacts/contacts-manage/contacts-manage.component";
 import { CourseSigninComponent } from "./courses/course-detail/course-signin/course-signin.component";
+import { ContactsListComponent } from "./shared/contacts-list/contacts-list.component";
 
 const routes: Routes = [
   {
@@ -41,7 +42,7 @@ const routes: Routes = [
         path: ":id",
         component: CourseDetailComponent,
         children: [
-          { path: "signin/:type", component: ContactsManageComponent },
+          { path: "signin/:type", component: ContactsListComponent },
           { path: "signin/:type/:contactId", component: CourseSigninComponent }
         ]
       }
